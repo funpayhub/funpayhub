0b26153c8815cae364f81eafa3428ca5c00e451b
qvvonk <qvvonk@gmail.com>
2025-09-03 12:28:27 +0300

    fphub

---
+++ funpayhub/app/hub.py
@@ -0,0 +1,7 @@
+from funpayhub.app.properties.properties import FunPayHubProperties
+
+class FunPayHub:
+    def __init__(self):
+        self.properties = FunPayHubProperties()
+        self.telegram = ...
+        self.funpay = ...
