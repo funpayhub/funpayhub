ed0799c0c13062960491df29e72ae9c445d5f991
qvvonk <qvvonk@gmail.com>
2025-10-08 12:49:29 +0300

    String and toogle parameter

--- funpayhub/lib/properties/parameter/string_parameter.py
+++ funpayhub/lib/properties/parameter/string_parameter.py
@@ -1,37 +1,29 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any
-from collections.abc import Callable
+from typing import Any
+from collections.abc import Callable, Awaitable
 
 from funpayhub.lib.properties.base import UNSET, _UNSET
-from funpayhub.lib.properties.parameter.base import CallableValue, MutableParameter
+from funpayhub.lib.properties.parameter.base import MutableParameter
 from funpayhub.lib.properties.parameter.converters import string_converter
-
-
-if TYPE_CHECKING:
-    from ..properties import Properties
 
 
 class StringParameter(MutableParameter[str]):
     def __init__(
         self,
         *,
-        properties: Properties,
         id: str,
-        name: CallableValue[str],
-        description: CallableValue[str],
-        default_value: CallableValue[str],
-        value: CallableValue[str] | _UNSET = UNSET,
-        validator: Callable[[str], str] | _UNSET = UNSET,
+        name: str,
+        description: str,
+        default_value: str,
+        validator: Callable[[str], Awaitable[None]] | _UNSET = UNSET,
         flags: set[Any] | None = None,
     ) -> None:
         super().__init__(
-            properties=properties,
             id=id,
             name=name,
             description=description,
             default_value=default_value,
-            value=value,
             validator=validator,
             converter=string_converter,
             flags=flags
--- funpayhub/lib/properties/parameter/toggle_parameter.py
+++ funpayhub/lib/properties/parameter/toggle_parameter.py
@@ -1,58 +1,43 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any
-from collections.abc import Callable
-
-from typing_extensions import Self
+from typing import Any
+from collections.abc import Callable, Awaitable
 
 from funpayhub.lib.properties.base import UNSET, _UNSET
-from funpayhub.lib.properties.parameter.base import CallableValue, MutableParameter
+from funpayhub.lib.properties.parameter.base import MutableParameter
 from funpayhub.lib.properties.parameter.converters import bool_converter
-
-
-if TYPE_CHECKING:
-    from ..properties import Properties
 
 
 class ToggleParameter(MutableParameter[bool]):
     def __init__(
         self,
         *,
-        properties: Properties,
         id: str,
-        name: CallableValue[str],
-        description: CallableValue[str],
-        default_value: CallableValue[bool],
-        value: CallableValue[bool] | _UNSET = UNSET,
-        validator: Callable[[bool], Any] | _UNSET = UNSET,
+        name: str,
+        description: str,
+        default_value: bool,
+        validator: Callable[[bool], Awaitable[None]] | _UNSET = UNSET,
         flags: set[Any] | None = None,
     ) -> None:
         super().__init__(
-            properties=properties,
             id=id,
             name=name,
             description=description,
             default_value=default_value,
-            value=value,
             validator=validator,
             converter=bool_converter,
             flags=flags,
         )
 
-    def on(self, save: bool = True) -> Self:
-        return self.set_value(True, skip_converter=True, save=save)
+    async def on(self, save: bool = True) -> None:
+        await self.set_value(True, skip_converter=True, save=save)
 
-    def off(self, save: bool = True) -> Self:
-        return self.set_value(False, skip_converter=True, save=save)
+    async def off(self, save: bool = True) -> None:
+        await self.set_value(False, skip_converter=True, save=save)
 
-    def toggle(self, save: bool = True) -> Self:
-        return self.set_value(not self.value, skip_converter=True, save=save)
+    async def toggle(self, save: bool = True) -> None:
+        await self.set_value(not self.value, skip_converter=True, save=save)
 
-    def __next__(self) -> bool:
-        # todo: delete
-        self.toggle(save=True)
-        return self.value
-
-    def next_value(self, save: bool = True) -> bool:
-        self.toggle(save=save)
+    async def next_value(self, save: bool = True) -> bool:
+        await self.toggle(save=save)
         return self.value
