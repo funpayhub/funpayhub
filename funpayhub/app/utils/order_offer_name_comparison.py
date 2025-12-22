40487be40ec5ae79ad51c4ffe9160c712b1392fa
qvvonk <qvvonk@gmail.com>
2025-12-22 13:31:10 +0300

    get_bought_amount

--- funpayhub/app/utils/order_offer_name_comparison.py
+++ funpayhub/app/utils/order_offer_name_comparison.py
@@ -2,10 +2,20 @@ from __future__ import annotations
 
 import re
 
-from funpaybotengine.types import OfferPreview, OrderPreview
 
+def get_bought_amount(offer_name: str, order_title: str) -> int:
+    """
+    Сопоставляет название лота с названием заказа.
 
-def is_valid_offer(offer: OfferPreview, order: OrderPreview) -> bool:
-    offer_name = re.escape(offer.name)
-    match = re.search(f'(^|(,\s+))({offer_name})($|(,\s+))', order.title)
-    return bool(match)
+    Если в названии заказа есть название лота, возвращает купленное кол-во.
+    В противном случае - возвращает 0.
+    """
+    offer_name = re.escape(offer_name)
+    match = re.search(
+        rf'(^|(,\s+))(?P<offername>{offer_name})($|\.$|(,\s+)).+(?P<amount>\d+ (шт|pcs)\.?,?)?.+',
+        order_title,
+    )
+    if not match:
+        return 0
+
+    return int(match.group('amount')) if match.group('amount') else 1
