fc00c613da05d65d4d55e3d99d623cb14911d702
qvvonk <qvvonk@gmail.com>
2026-01-13 09:51:14 +0300

    updater

--- updater.py
+++ updater.py
@@ -3,11 +3,16 @@ from aiohttp import ClientSession
 from packaging.version import Version
 from dataclasses import dataclass
 import os
+import zipfile
+from pathlib import Path
+import shutil
 
 
 REPO = 'funpayhub/funpayhub'
 CURRENT_VERSION = Version('0.1.2')
 UPDATE_EXCLUDE = ['config', 'logs', 'plugins', 'storage']
+RELEASES_PATH = Path(os.environ.get('RELEASES_PATH', 'releases'))
+UPDATE_PATH = RELEASES_PATH / '.update'
 
 
 @dataclass
@@ -29,17 +34,18 @@ async def check_updates() -> UpdateInfo | None:
     if not data:
         return None
 
-    latest = data[-1]
+    latest = data[0]
     latest_version = latest['tag_name'].replace('v', '').strip()
     if not latest_version:
+        print('No new version available.')
         return None
 
     latest_version = Version(latest_version)
     return UpdateInfo(
         version=latest_version,
-        title=data['name'],
-        description=data['body'],
-        url=data['zipball_url']
+        title=latest['name'],
+        description=latest['body'],
+        url=latest['zipball_url']
     )
 
 
@@ -62,16 +68,31 @@ async def download_update(url: str) -> None:
         print('timeout')
 
 
-def install_update(update_path: str) -> None:
-    # Скопировать старые файлы в 'fph.backup'
-    # Распаковать новые файлы
-    # Удалить ненужные файлы
-    # Установить зависимости
-    # Удалить backup
-    # перезапуститься
-    ...
+def install_update(update_archive: str) -> None:
+    if os.path.exists(UPDATE_PATH):
+        shutil.rmtree(UPDATE_PATH)
+    os.makedirs(UPDATE_PATH, exist_ok=True)
+
+    with zipfile.ZipFile(update_archive, mode='r') as zip:
+        for info in zip.infolist():
+            if info.is_dir():
+                continue
+
+            new_path = UPDATE_PATH / Path(*Path(info.filename).parts[1:])
+            os.makedirs(new_path.parent, exist_ok=True)
+            with zip.open(info.filename, 'r') as src, open(new_path, 'wb') as dst:
+                shutil.copyfileobj(src, dst)
+
+
+async def main():
+    update = await check_updates()
+    if not update:
+        return
+
+    await download_update(update.url)
+    install_update('.update.zip')
 
 
 if __name__ == '__main__':
     import asyncio
-    asyncio.run(check_updates())
+    asyncio.run(main())
