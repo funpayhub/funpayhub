b00645301466f29d9753609e55f160a7812bd31f
qvvonk <qvvonk@gmail.com>
2026-01-15 14:10:29 +0300

    plugins scratch

---
+++ funpayhub/lib/plugins/manager.py
@@ -0,0 +1,217 @@
+from __future__ import annotations
+
+from collections.abc import Callable, Awaitable
+from typing import TYPE_CHECKING
+from types import MappingProxyType
+from pathlib import Path
+from packaging.version import Version
+import sys
+from typing import Any
+import os
+import keyword
+import importlib
+from contextlib import suppress
+from .types import PluginManifest, Plugin, LoadedPlugin
+from funpayhub.app.dispatching import Router as HUBRouter, Dispatcher as HUBDispatcher
+from funpaybotengine import Router as FPRouter, Dispatcher as FPDispatcher
+from aiogram import Router as TGRouter, Dispatcher as TGDispatcher
+from funpayhub.lib.telegram.ui import MenuBuilder, MenuModification, ButtonBuilder, ButtonModification
+
+if TYPE_CHECKING:
+    from funpayhub.app.main import FunPayHub
+
+
+class PluginManager:
+    PLUGINS_PATH = Path(os.getcwd()) / 'plugins'
+    DEV_PLUGINS_PATH = Path(os.environ.get('PYTHONPATH', os.getcwd())) / 'plugins'
+
+    def __init__(self, hub: FunPayHub):
+        self._disabled_plugins: set[str] = set()
+        self._plugins: dict[str, LoadedPlugin] = {}
+        self._hub = hub
+
+        paths = [str(self.DEV_PLUGINS_PATH), str(self.PLUGINS_PATH)]
+        for i in paths:
+            if i not in sys.path:
+                sys.path.append(i)
+
+    def load_disabled_plugins(self, path: str) -> None:
+        with open(path, 'r', encoding='utf-8') as f:
+            for i in f.read().splitlines():
+                if not i:
+                    continue
+                self._disabled_plugins.add(i)
+
+    def disable_plugin(self, plugin_id: str) -> None:
+        if not plugin_id:
+            return
+
+        self._disabled_plugins.add(plugin_id)
+
+        with open('disabled_plugins', 'w', encoding='utf-8') as f:
+            f.write('\n'.join(self._disabled_plugins))
+
+    def enable_plugin(self, plugin_id: str) -> None:
+        if not plugin_id:
+            return
+
+        self._disabled_plugins.discard(plugin_id)
+
+        with open('disabled_plugins', 'w', encoding='utf-8') as f:
+            f.write('\n'.join(self._disabled_plugins))
+
+    def load_plugin(self, plugin_path: str | Path) -> None:
+        module_name = Path(plugin_path).name
+        if not module_name.isidentifier() or keyword.iskeyword(module_name):
+            return # todo
+
+        try:
+            manifest = self._load_plugin_manifest(plugin_path)
+        except:
+            return # todo
+
+        if manifest.plugin_id in self._disabled_plugins:
+            return  # todo: log
+
+        if manifest.plugin_id in self._plugins:
+            return # todo: log
+
+        if Version(self.hub.properties.version.value) not in manifest.hub_version:
+            return # todo
+
+        try:
+            plugin_instance = self._load_entry_point(plugin_path, manifest)
+        except:
+            return # todo: log
+
+        plugin = LoadedPlugin(manifest=manifest, plugin=plugin_instance)
+        self._plugins[manifest.plugin_id] = plugin
+
+    def _load_plugin_manifest(self, plugin_path: str | Path) -> PluginManifest:
+        if not (plugin_path / 'manifest.json').exists():
+            raise FileNotFoundError()
+
+        with open(plugin_path / 'manifest.json', 'r', encoding='utf-8') as f:
+            return PluginManifest.model_validate_json(f.read())
+
+    def _load_entry_point(
+        self,
+        plugin_path: str | Path,
+        manifest: PluginManifest
+    ) -> Plugin:
+        plugin_module_name = Path(plugin_path).name
+        module_name, class_name = manifest.entry_point.rsplit('.', 1)
+
+        module = importlib.import_module(f'{plugin_module_name}.{module_name}')
+        plugin_class: type[Plugin] | None = getattr(module, class_name, None)
+
+        if plugin_class is None:
+            raise Exception()  # todo
+
+        if not issubclass(plugin_class, Plugin):
+            raise Exception()  # todo
+
+        return plugin_class(manifest, self._hub)
+
+
+    async def _run_pre_setup_step(self, plugin: LoadedPlugin) -> None:
+        await self._run_general_step(plugin.plugin.pre_setup)
+
+    async def _apply_properties(self, plugin: LoadedPlugin) -> None:
+        try:
+            properties = await plugin.plugin.properties()
+        except NotImplementedError:
+            return
+
+        self.hub.properties.attach_properties(properties)
+        await self.hub.properties.load()
+        self.plugins[plugin.manifest.plugin_id].properties = properties
+
+    async def _run_setup_properties_step(self, plugin: LoadedPlugin) -> None:
+        await self._run_general_step(plugin.plugin.setup_properties)
+        await self.hub.properties.load()
+
+    async def _apply_hub_routers(self, plugin: LoadedPlugin) -> None:
+        try:
+            routers = await plugin.plugin.hub_routers()
+        except NotImplementedError:
+            return
+
+        if isinstance(routers, HUBRouter):
+            routers = [routers]
+        elif isinstance(routers, HUBDispatcher):
+            raise TypeError('Plugin router cannot be a Dispatcher.')
+        elif not isinstance(routers, list):
+            raise TypeError('Plugin.hub_routers must return a `Router` or a list of `Router`.')
+
+        self.hub.dispatcher.connect_routers(*routers)
+
+    async def _run_setup_hub_routers_step(self, plugin: LoadedPlugin) -> None:
+        await self._run_general_step(plugin.plugin.setup_hub_routers)
+
+    async def _apply_funpay_routers(self, plugin: LoadedPlugin) -> None:
+        try:
+            routers = await plugin.plugin.funpay_routers()
+        except NotImplementedError:
+            return
+
+        if isinstance(routers, FPRouter):
+            routers = [routers]
+        elif isinstance(routers, FPDispatcher):
+            raise TypeError('Plugin router cannot be a Dispatcher.')
+        elif not isinstance(routers, list):
+            raise TypeError('Plugin.hub_routers must return a `Router` or a list of `Router`.')
+
+        self.hub.funpay.dispatcher.connect_routers(*routers)
+
+    async def _run_setup_funpayhub_routers_step(self, plugin: LoadedPlugin) -> None:
+        await self._run_general_step(plugin.plugin.setup_funpay_routers)
+
+    async def _apply_telegram_routers(self, plugin: LoadedPlugin) -> None:
+        try:
+            routers = await plugin.plugin.telegram_routers()
+        except NotImplementedError:
+            return
+
+        if isinstance(routers, TGRouter):
+            routers = [routers]
+        elif isinstance(routers, TGDispatcher):
+            raise TypeError('Plugin router cannot be a Dispatcher.')
+        elif not isinstance(routers, list):
+            raise TypeError('Plugin.hub_routers must return a `Router` or a list of `Router`.')
+
+        self.hub.telegram.dispatcher.include_routers(*routers)
+
+    async def _run_setup_telegram_routers_step(self, plugin: LoadedPlugin) -> None:
+        await self._run_general_step(plugin.plugin.setup_telegram_routers)
+
+    async def _apply_menus(self, plugin: LoadedPlugin) -> None:
+        try:
+            menus = await plugin.plugin.menus()
+        except NotImplementedError:
+            return
+
+        if issubclass(menus, MenuBuilder):
+            menus = [menus]
+        elif not isinstance(menus, list):
+            raise TypeError('Plugin.menus must return a `MenuBuilder` type or a list of `MenuBuilder` types.')
+
+        for i in menus:
+            if not issubclass(menus, MenuBuilder):
+                ...
+
+    async def _run_general_step(self, step: Callable[[], Awaitable[Any]]):
+        with suppress(NotImplementedError):
+            await step()
+
+    @property
+    def hub(self) -> FunPayHub:
+        return self._hub
+
+    @property
+    def plugins(self) -> MappingProxyType[str, LoadedPlugin]:
+        return MappingProxyType(self._plugins)
+
+    @property
+    def disabled_plugins(self) -> frozenset[str]:
+        return frozenset(self._disabled_plugins)
---
+++ funpayhub/lib/plugins/types.py
@@ -0,0 +1,131 @@
+from __future__ import annotations
+
+
+__all__ = [
+    'PluginManifest',
+    'Plugin',
+    'LoadedPlugin'
+]
+
+
+from pydantic import BaseModel, Field, field_validator
+from funpayhub.lib.properties import Properties
+from funpayhub.app.dispatching import Router
+from funpayhub.lib.telegram.ui import MenuBuilder, ButtonBuilder, MenuModification, ButtonModification
+from packaging.version import Version
+from packaging.specifiers import SpecifierSet
+from typing import TYPE_CHECKING
+from dataclasses import dataclass
+
+
+if TYPE_CHECKING:
+    from funpayhub.app.main import FunPayHub
+
+
+class PluginManifest(BaseModel):
+    model_config = {
+        'extra': 'allow',
+        'frozen': True,
+    }
+
+    manifest: Version
+    plugin_version: Version
+    hub_version: SpecifierSet
+    plugin_id: str
+    name: str
+    description: str = Field(default='')
+    entry_point: str = Field(pattern=r'^([a-zA-Z_][a-zA-Z0-9_]*\.)+[a-zA-Z_][a-zA-Z0-9_]*$')
+    dependencies: list[str] = Field(default_factory=list)
+    plugin_dependencies: list[str] = Field(default_factory=list)
+    locales_path: list[str] = Field(default_factory=list)
+
+    @field_validator('plugin_version', mode='before')
+    def convert_version(self, value: str | Version) -> Version:
+        if isinstance(value, str):
+            value = Version(value)
+        return value
+
+    @field_validator('hub_version', mode='before')
+    def convert_hub_version(self, value: str | SpecifierSet) -> SpecifierSet:
+        if isinstance(value, str):
+            value = SpecifierSet(value)
+        return value
+
+
+class Plugin:
+    def __init__(
+        self,
+        manifest: PluginManifest,
+        hub: FunPayHub
+    ) -> None:
+        self._manifest = manifest
+        self._hub = hub
+
+    @property
+    def manifest(self) -> PluginManifest:
+        return self._manifest
+
+    @property
+    def hub(self) -> FunPayHub:
+        return self._hub
+
+    async def pre_setup(self) -> Properties:
+        raise NotImplementedError()
+
+    async def properties(self) -> Properties:
+        raise NotImplementedError()
+
+    async def setup_properties(self) -> None:
+        raise NotImplementedError()
+
+    async def hub_routers(self) -> Router | list[Router]:
+        raise NotImplementedError()
+
+    async def setup_hub_routers(self) -> None:
+        raise NotImplementedError()
+
+    async def funpay_routers(self) -> Router | list[Router]:
+        raise NotImplementedError()
+
+    async def setup_funpay_routers(self) -> None:
+        raise NotImplementedError()
+
+    async def telegram_routers(self) -> Router | list[Router]:
+        raise NotImplementedError()
+
+    async def setup_telegram_routers(self) -> None:
+        raise NotImplementedError()
+
+    async def menus(self) -> type[MenuBuilder] | list[type[MenuBuilder]]:
+        raise NotImplementedError()
+
+    async def setup_menus(self) -> None:
+        raise NotImplementedError()
+
+    async def buttons(self) -> ButtonBuilder | list[type[ButtonBuilder]]:
+        raise NotImplementedError()
+
+    async def setup_buttons(self) -> None:
+        raise NotImplementedError()
+
+    async def menu_modifications(self) -> dict[str, type[MenuModification] | list[type[MenuModification]]]:
+        raise NotImplementedError()
+
+    async def setup_menu_modifications(self) -> None:
+        raise NotImplementedError()
+
+    async def button_modifications(self) -> dict[str, type[ButtonModification] | list[type[ButtonModification]]]:
+        raise NotImplementedError()
+
+    async def setup_button_modifications(self) -> None:
+        raise NotImplementedError()
+
+    async def post_setup(self) -> None:
+        raise NotImplementedError()
+
+
+@dataclass
+class LoadedPlugin:
+    manifest: PluginManifest
+    plugin: Plugin
+    properties: Properties | None = None
