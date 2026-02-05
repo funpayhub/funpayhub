af6e10dfab5ecb85e60bc9ace2b5026a55bb2055
qvvonk <qvvonk@gmail.com>
2026-02-05 13:30:13 +0300

    ui ids

--- funpayhub/app/telegram/ui/ids.py
+++ funpayhub/app/telegram/ui/ids.py
@@ -1,29 +1,21 @@
 from __future__ import annotations
 
-from enum import UNIQUE, StrEnum, verify
+from funpayhub.lib.base_app.telegram import MenuIds as BaseMenuIds, ButtonIds as BaseButtonIds
 
 
-@verify(UNIQUE)
-class MenuIds(StrEnum):
+class MenuIds(BaseMenuIds):
     state_menu = 'fph:state_menu'
     main_menu = 'fph:main_menu'
-    properties_entry = 'fph:properties_entry'
-    param_value_manual_input = 'fph:param_value_manual_input'
     formatters_list = 'fph:formatters_list'
     formatter_info = 'fph:formatter_info'
     hooks_list = 'fph:hooks_list'
     hook_info = 'fph:hook_info'
     tg_chat_notifications = 'fph:tg_chat_notifications'
-    add_list_item = 'fph:add_list_item'
     add_command = 'fph:add_command'
     control = 'fph:control'
 
     new_funpay_message = 'fph:new_funpay_message'
     send_funpay_message = 'fph:send_funpay_message'
-
-    properties_properties = 'fph:properties_properties'
-    properties_list_param = 'fph:properties_list_param'
-    properties_choice_param = 'fph:properties_choice_param'
 
     start_notification = 'fph:start_notification'
     funpay_start_notification = 'fph:funpay_start_notification'
@@ -41,8 +33,5 @@ class MenuIds(StrEnum):
     add_auto_delivery_rule = 'fph:add_auto_delivery_rule'
 
 
-class ButtonIds:
-    properties_entry = 'fph:properties_entry'
-    properties_toggle_param = 'fph:properties_toggle_param'
-    properties_change_param_value = 'fph:properties_change_param_value'
-    properties_open_param_menu = 'fph:properties_open_param_menu'
+class ButtonIds(BaseButtonIds):
+    ...
--- funpayhub/lib/base_app/telegram/__init__.py
+++ funpayhub/lib/base_app/telegram/__init__.py
@@ -3,6 +3,17 @@ from __future__ import annotations
 
 __all__ = [
     'TelegramApp',
+    'MenuIds',
+    'ButtonIds',
 ]
 
 from .main import TelegramApp
+from .app.properties.ui import NodeMenuIds, NodeButtonIds
+
+
+class MenuIds(NodeMenuIds):
+    ...
+
+
+class ButtonIds(NodeButtonIds):
+    ...
