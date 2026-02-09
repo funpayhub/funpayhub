fea104cb5b1e1bbb12218c0ff18735f8d53319f8
qvvonk <qvvonk@gmail.com>
2026-02-09 16:31:55 +0300

    small fix

--- funpayhub/app/utils/get_profile_categories.py
+++ funpayhub/app/utils/get_profile_categories.py
@@ -10,6 +10,11 @@ from funpaybotengine.types.pages import ProfilePage
 
 
 async def get_profile_raisable_categories(profile: ProfilePage, bot: Bot) -> set[int]:
+    result = set()
+
+    if not profile.offers.get(SubcategoryType.COMMON):
+        return result
+
     categories = await bot.storage.get_categories()
     subcategories_to_category = {
         subcat.id: cat
@@ -17,7 +22,7 @@ async def get_profile_raisable_categories(profile: ProfilePage, bot: Bot) -> set
         for subcat in cat.subcategories
         if subcat.type is SubcategoryType.COMMON
     }
-    result = set()
+
     for subcategory_id in profile.offers[SubcategoryType.COMMON]:
         if subcategory_id in subcategories_to_category:
             result.add(subcategories_to_category[subcategory_id].id)
