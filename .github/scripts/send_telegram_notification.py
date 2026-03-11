eb414221b295f1e4e7e55ab61f60d79a0930b181
qvvonk <qvvonk@gmail.com>
2026-03-12 00:52:49 +0300

    workflow fix

--- .github/scripts/send_telegram_notification.py
+++ .github/scripts/send_telegram_notification.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import os
+import re
 import json
 import asyncio
 
@@ -18,12 +19,20 @@ with open(os.environ['GITHUB_EVENT_PATH'], 'r') as f:
     event = json.load(f)
 
 
+def escape_md(text: str) -> str:
+    special_chars = r'_*[]()~`>#+-=|{}.!'
+    return re.sub(f'([{re.escape(special_chars)}])', r'\\\1', text)
+
+
 async def send_notification():
     message = (
-        f'**🎉 Новый релиз FunPay Hub: {event["release"]["tag_name"]}**\n\n'
+        '**' + escape_md(f'🎉 Новый релиз FunPay Hub: {event["release"]["tag_name"]}') + '**\n\n'
         f'{event["release"]["body"]}\n\n'
-        f'🔗 Ссылка на релиз: {event["release"]["html_url"]}\n'
-        f'**♻ ️Чтобы обновиться: /menu -> 🔌 Системное меню -> 🔍 Проверить наличие обновлений.**'
+        '**' + escape_md(f'🔗 Ссылка на релиз: {event["release"]["html_url"]}') + '**\n\n'
+        '**'
+        + escape_md(
+            '♻ ️Чтобы обновиться: /menu -> 🔌 Системное меню -> 🔍 Проверить наличие обновлений.'
+        )
     )
 
     msg = await bot.send_message(-1002290367706, message, message_thread_id=None)
