27cbd1b04b0a02ca37ca8b71b8f99f53fe0e3e19
qvvonk <qvvonk@gmail.com>
2026-03-19 16:49:08 +0300

    goods sources ui

--- funpayhub/app/telegram/modules/goods/ui.py
+++ funpayhub/app/telegram/modules/goods/ui.py
@@ -2,9 +2,9 @@ from __future__ import annotations
 
 import html
 from typing import TYPE_CHECKING
-from dataclasses import dataclass
 
-from funpayhub.lib.telegram.ui import Menu, Button, MenuBuilder, MenuContextOld, KeyboardBuilder
+from funpayhub.lib.translater import translater
+from funpayhub.lib.telegram.ui import Menu, Button, MenuBuilder, MenuContext, KeyboardBuilder
 from funpayhub.lib.base_app.telegram.app.ui.callbacks import OpenMenu
 from funpayhub.lib.base_app.telegram.app.ui.ui_finalizers import (
     StripAndNavigationFinalizer,
@@ -18,26 +18,22 @@ from . import callbacks as cbs
 
 
 if TYPE_CHECKING:
-    from funpayhub.lib.translater import Translater as Tr
     from funpayhub.lib.goods_sources import GoodsSource, GoodsSourcesManager
 
 
-@dataclass(kw_only=True)
-class GoodsInfoMenuContext(MenuContextOld):
+ru = translater.translate
+
+
+class GoodsInfoMenuContext(MenuContext):
     source_id: str
 
 
 class GoodsSourcesListMenuBuilder(
     MenuBuilder,
     menu_id=MenuIds.goods_sources_list,
-    context_type=MenuContextOld,
+    context_type=MenuContext,
 ):
-    async def build(
-        self,
-        ctx: MenuContextOld,
-        goods_manager: GoodsSourcesManager,
-        translater: Tr,
-    ) -> Menu:
+    async def build(self, ctx: MenuContext, goods_manager: GoodsSourcesManager) -> Menu:
         kb = KeyboardBuilder()
         for source in goods_manager.values():
             kb.add_callback_button(
@@ -46,19 +42,19 @@ class GoodsSourcesListMenuBuilder(
                 callback_data=OpenMenu(
                     menu_id=MenuIds.goods_source_info,
                     context_data={'source_id': source.source_id},
-                    from_callback=ctx.callback_data,
+                    ui_history=ctx.as_ui_history(),
                 ).pack(),
             )
 
         footer_kb = KeyboardBuilder()
         footer_kb.add_callback_button(
             button_id='add_text_source',
-            text=translater.translate('➕ Добавить файл с товарами'),
-            callback_data=cbs.AddGoodsTxtSource(from_callback=ctx.callback_data).pack(),
+            text=ru('➕ Добавить файл с товарами'),
+            callback_data=cbs.AddGoodsTxtSource(ui_history=ctx.as_ui_history()).pack(),
         )
 
         return Menu(
-            main_text=translater.translate('🗳 Источники товаров'),
+            main_text=ru('🗳 Источники товаров'),
             main_keyboard=kb,
             footer_keyboard=footer_kb,
             finalizer=StripAndNavigationFinalizer(),
@@ -70,30 +66,25 @@ class GoodsSourceInfoMenuBuilder(
     menu_id=MenuIds.goods_source_info,
     context_type=GoodsInfoMenuContext,
 ):
-    async def build(
-        self,
-        ctx: GoodsInfoMenuContext,
-        goods_manager: GoodsSourcesManager,
-        translater: Tr,
-    ) -> Menu:
+    async def build(self, ctx: GoodsInfoMenuContext, goods_manager: GoodsSourcesManager) -> Menu:
         source = goods_manager[ctx.source_id]
         kb = KeyboardBuilder()
 
         kb.add_row(
             Button.callback_button(
                 button_id='download_goods',
-                text=translater.translate('⤵ Скачать товары'),
+                text=ru('⤵ Скачать товары'),
                 callback_data=cbs.DownloadGoods(
                     source_id=source.source_id,
-                    from_callback=ctx.callback_data,
+                    ui_history=ctx.as_ui_history(),
                 ).pack(),
             ),
             Button.callback_button(
                 button_id='upload_goods',
-                text=translater.translate('⤴ Выгрузить товары'),
+                text=ru('⤴ Выгрузить товары'),
                 callback_data=cbs.UploadGoods(
                     source_id=source.source_id,
-                    from_callback=ctx.callback_data,
+                    ui_history=ctx.as_ui_history(),
                 ).pack(),
             ),
         )
@@ -101,48 +92,46 @@ class GoodsSourceInfoMenuBuilder(
         kb.add_row(
             Button.callback_button(
                 button_id='add_goods',
-                text=translater.translate('➕ Добавить товары'),
+                text=ru('➕ Добавить товары'),
                 callback_data=cbs.AddGoods(
                     source_id=source.source_id,
-                    from_callback=ctx.callback_data,
+                    ui_history=ctx.as_ui_history(),
                 ).pack(),
             ),
             Button.callback_button(
                 button_id='remove_goods',
-                text=translater.translate('🗑️ Удалить товары'),
+                text=ru('🗑️ Удалить товары'),
                 callback_data=cbs.RemoveGoods(
                     source_id=source.source_id,
-                    from_callback=ctx.callback_data,
+                    ui_history=ctx.as_ui_history(),
                 ).pack(),
             ),
         )
 
         kb.add_callback_button(
             button_id='reload_source',
-            text=translater.translate('🔄 Перезагрузить источник'),
+            text=ru('🔄 Перезагрузить источник'),
             callback_data=cbs.ReloadGoodsSource(
                 source_id=source.source_id,
-                from_callback=ctx.callback_data,
+                ui_history=ctx.as_ui_history(),
             ).pack(),
         )
 
-        goods_text, min_index, max_index = await self._generate_text(
-            source,
-            translater,
-            ctx.view_page,
-        )
-        text = translater.translate(
-            '🗳 Источник товаров <b><i>{goods_source_id}</i></b>.\n\n📊 Кол-во товаров: <b><i>{goods_amount}</i></b>.\n🧩 Тип источника: <b><i>{goods_source_type}</i></b>.\n🧭 Источник: <code class="language-Источник">{goods_source}</code>',
-        ).format(
+        goods_text, min_index, max_index = await self._generate_text(source, ctx.view_page)
+        text = ru(
+            '🗳 Источник товаров <b><i>{goods_source_id}</i></b>.\n\n'
+            '📊 Кол-во товаров: <b><i>{goods_amount}</i></b>.\n'
+            '🧩 Тип источника: <b><i>{goods_source_type}</i></b>.\n'
+            '🧭 Источник: <code class="language-Источник">{goods_source}</code>',
             goods_source_id=source.display_id,
             goods_amount=len(source),
-            goods_source_type=translater.translate(source.display_source_type),
-            goods_source=translater.translate(source.display_source),
+            goods_source_type=ru(source.display_source_type),
+            goods_source=ru(source.display_source),
         )
         text += '\n\n'
         text += (
             f'<pre>'
-            f'<code class="language-{translater.translate("Товары")} '
+            f'<code class="language-{ru("Товары")} '
             f'{min_index + 1}-{max_index + 1}">'
             f'{goods_text}'
             f'</code></pre>\n'
@@ -155,12 +144,7 @@ class GoodsSourceInfoMenuBuilder(
             finalizer=StripAndNavigationFinalizer(),
         )
 
-    async def _generate_text(
-        self,
-        source: GoodsSource,
-        translater: Tr,
-        page: int = 0,
-    ) -> tuple[str, int, int]:
+    async def _generate_text(self, source: GoodsSource, page: int = 0) -> tuple[str, int, int]:
         MAX_LINE_LEN = 120
         MAX_LINES = 30
         START = page * MAX_LINES
@@ -174,7 +158,7 @@ class GoodsSourceInfoMenuBuilder(
         for index, product in enumerate(goods_batch):
             product = html.escape(product).strip()
             if len(product) > MAX_LINE_LEN:
-                product = translater.translate('❌ Товар слишком длинный.')
+                product = ru('❌ Товар слишком длинный.')
 
             line = f'{START + index + 1}. {product}\n'
             text += line
@@ -186,9 +170,6 @@ class AddRemoveButtonToGoodsSourceInfoModification(
     AddRemoveButtonBaseModification,
     modification_id='fph:add_remove_button_to_goods_source_info',
 ):
-    async def modify(self, ctx: GoodsInfoMenuContext, menu: Menu, translater: Tr) -> Menu:
-        delete_callback = cbs.RemoveGoodsSource(
-            source_id=ctx.source_id,
-            from_callback=ctx.callback_data,
-        ).pack()
-        return await self._modify(ctx, menu, translater, delete_callback=delete_callback)
+    async def modify(self, ctx: GoodsInfoMenuContext, menu: Menu) -> Menu:
+        cb = cbs.RemoveGoodsSource(source_id=ctx.source_id, ui_history=ctx.as_ui_history()).pack()
+        return await self._modify(ctx, menu, 'delete_source', cb)
