183ea3bd4d79435e9c668e1cff7b2c3171e687bb
qvvonk <qvvonk@gmail.com>
2026-03-19 20:25:26 +0300

    small changes

--- funpayhub/app/telegram/modules/updates/router.py
+++ funpayhub/app/telegram/modules/updates/router.py
@@ -1,13 +1,14 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 from asyncio import Lock
 
 from aiogram import Router
-from aiogram.types import CallbackQuery
 
 from funpayhub import exit_codes
 from funpayhub.updater import check_updates, install_update, download_update
+
+from funpayhub.lib.translater import translater
 
 from funpayhub.app.telegram.ui.ids import MenuIds
 
@@ -16,107 +17,74 @@ from .ui import UpdateMenuContext, InstallUpdateMenuContext
 
 
 if TYPE_CHECKING:
-    from funpayhub.lib.translater import Translater as Tr
-    from funpayhub.lib.telegram.ui.registry import UIRegistry as UI
+    from aiogram.types import CallbackQuery as Query
 
     from funpayhub.app.main import FunPayHub as FPH
 
 
 router = r = Router(name='fph:updates')
+ru = translater.translate
 updating_lock = Lock()
 
 
 @r.callback_query(cbs.CheckForUpdates.filter())
-async def check_for_updates(query: CallbackQuery, tg_ui: UI, hub: FPH, translater: Tr) -> None:
+async def check_for_updates(q: Query, hub: FPH) -> Any:
     try:
         update = await check_updates(hub.properties.version.value)
     except:
-        await query.answer(
-            translater.translate(
-                '❌ Произошла ошибка при получении данных об обновлениях.\nПодробности в лог файле.',
-            ),
+        return q.answer(
+            ru('❌ Ошибка получения данных обновления.\nПодробности в лог файле.'),
             show_alert=True,
         )
-        return
 
-    await UpdateMenuContext(
-        menu_id=MenuIds.update,
-        trigger=query,
-        update_info=update,
-    ).build_and_answer(tg_ui, query.message)
-    await query.answer()
+    await UpdateMenuContext(menu_id=MenuIds.update, trigger=q, update_info=update).answer_to()
 
 
 @r.callback_query(cbs.DownloadUpdate.filter())
-async def download_upd(
-    query: CallbackQuery,
-    tg_ui: UI,
-    callback_data: cbs.DownloadUpdate,
-    hub: FPH,
-    translater: Tr,
-) -> None:
+async def download_upd(q: Query, cbd: cbs.DownloadUpdate, hub: FPH) -> Any:
     if updating_lock.locked():
-        await query.answer(
-            translater.translate(
-                '❌ Процесс установки обновления уже запущен. Пожалуйста, подождите.',
-            ),
+        return q.answer(
+            ru('❌ Процесс установки обновления уже запущен. Пожалуйста, подождите.'),
             show_alert=True,
         )
-        return
 
     async with updating_lock:
         try:
-            await download_update(callback_data.url)
+            await download_update(cbd.url)
         except:
-            await query.answer(
-                translater.translate(
-                    '❌ Произошла ошибка при скачивании обновления.\nПодробности в лог файле.',
-                ),
+            return q.answer(
+                ru('❌ Ошика скачивания обновления.\nПодробности в лог файле.'),
                 show_alert=True,
             )
-            return
 
     await InstallUpdateMenuContext(
         menu_id=MenuIds.install_update,
-        trigger=query,
+        trigger=q,
         instance_id=hub.instance_id,
-    ).build_and_apply(tg_ui, query.message)
+    ).apply_to()
 
 
 @r.callback_query(cbs.InstallUpdate.filter())
-async def install_upd(
-    query: CallbackQuery,
-    callback_data: cbs.InstallUpdate,
-    hub: FPH,
-    translater: Tr,
-) -> None:
-    if callback_data.instance_id != hub.instance_id:
-        await query.answer(
-            translater.translate('❌ Эта кнопка устарела :(\nОткройте меню заного.'),
-            show_alert=True,
+async def install_upd(query: Query, cbd: cbs.InstallUpdate, hub: FPH) -> Any:
+    if cbd.instance_id != hub.instance_id:
+        return query.answer(
+            ru('❌ Эта кнопка устарела :(\nОткройте меню заного.'), show_alert=True
         )
-        return
 
     if updating_lock.locked():
-        await query.answer(
-            translater.translate(
-                '❌ Процесс установки обновления уже запущен. Пожалуйста, подождите.',
-            ),
+        return query.answer(
+            ru('❌ Процесс установки обновления уже запущен. Пожалуйста, подождите.'),
             show_alert=True,
         )
-        return
 
     async with updating_lock:
         try:
             install_update('.update.zip')
         except:
-            await query.answer(
-                translater.translate(
-                    '❌ Произошла ошибка при распаковке обновления.\nПодробнее в лог файле.',
-                ),
+            return query.answer(
+                ru('❌ Ошибка распаковки обновления.\nПодробнее в лог файле.'),
                 show_alert=True,
             )
-            return
 
-    await query.message.edit_text(translater.translate('⏳ Установка обновления ...'))
+    await query.message.edit_text(ru('<b>⏳ Установка обновления ...</b>'))
     await hub.shutdown(exit_codes.UPDATE)
--- funpayhub/app/telegram/modules/updates/ui.py
+++ funpayhub/app/telegram/modules/updates/ui.py
@@ -11,9 +11,9 @@ __all__ = [
 
 import html
 from typing import TYPE_CHECKING
-from dataclasses import dataclass
 
-from funpayhub.lib.telegram.ui import Menu, MenuBuilder, MenuContextOld, KeyboardBuilder
+from funpayhub.lib.translater import translater
+from funpayhub.lib.telegram.ui import Menu, MenuBuilder, MenuContext, KeyboardBuilder
 from funpayhub.lib.base_app.telegram.app.ui.ui_finalizers import StripAndNavigationFinalizer
 
 from funpayhub.app.telegram.ui.ids import MenuIds
@@ -24,46 +24,47 @@ from . import callbacks as cbs
 if TYPE_CHECKING:
     from funpayhub.updater import UpdateInfo
 
-    from funpayhub.lib.translater import Translater as Tr
-
-    from funpayhub.app.main import FunPayHub
+    from funpayhub.app.main import FunPayHub as FPH
 
 
-@dataclass(kw_only=True)
-class UpdateMenuContext(MenuContextOld):
+ru = translater.translate
+
+
+class UpdateMenuContext(MenuContext):
     update_info: UpdateInfo | None = None
 
 
-@dataclass(kw_only=True)
-class InstallUpdateMenuContext(MenuContextOld):
+class InstallUpdateMenuContext(MenuContext):
     instance_id: str
 
 
 class UpdateMenuBuilder(MenuBuilder, menu_id=MenuIds.update, context_type=UpdateMenuContext):
-    async def build(self, ctx: UpdateMenuContext, translater: Tr) -> Menu:
+    async def build(self, ctx: UpdateMenuContext) -> Menu:
         menu = Menu(finalizer=StripAndNavigationFinalizer())
         if ctx.update_info is None:
-            menu.main_text = translater.translate('✅ Установлена последняя версия FunPay Hub.')
+            menu.main_text = ru('<b>✅ Установлена последняя версия FunPay Hub.</b>')
             return menu
 
         desc = html.escape(ctx.update_info.description)
         if len(desc) > 3000:
             desc = (
-                f'{desc[:3000]}...\n\n{translater.translate("Полный список изменений")}'
+                f'{desc[:3000]}...\n\n{ru("Полный список изменений")}'
                 ': https://github.com/funpayhub/funpayhub/releases/latest'
             )
 
         menu.main_text = (
-            f'{translater.translate("🔥 <b>Доступно новое обновление</b>!")}\n\n'
+            f'{ru("<b>🔥 Доступно новое обновление</b>!")}\n\n'
             f'<b>{html.escape(ctx.update_info.title)}</b>\n\n'
             f'{desc}'
         )
 
-        menu.main_keyboard = KeyboardBuilder()
         menu.main_keyboard.add_callback_button(
             button_id='download_update',
-            text=translater.translate('🔽 Скачать обновление'),
-            callback_data=cbs.DownloadUpdate(url=ctx.update_info.url).pack(),
+            text=ru('🔽 Скачать обновление'),
+            callback_data=cbs.DownloadUpdate(
+                url=ctx.update_info.url,
+                ui_history=ctx.as_ui_history(),
+            ).pack(),
         )
 
         return menu
@@ -74,15 +75,15 @@ class InstallUpdateMenuBuilder(
     menu_id=MenuIds.install_update,
     context_type=InstallUpdateMenuContext,
 ):
-    async def build(self, ctx: InstallUpdateMenuContext, translater: Tr, hub: FunPayHub) -> Menu:
+    async def build(self, ctx: InstallUpdateMenuContext, hub: FPH) -> Menu:
         kb = KeyboardBuilder()
         kb.add_callback_button(
             button_id='install_update',
-            text=translater.translate('⏬ Установить обновление'),
+            text=ru('⏬ Установить обновление'),
             callback_data=cbs.InstallUpdate(instance_id=hub.instance_id).pack_compact(),
         )
 
         return Menu(
-            main_text=translater.translate('✅ Обновление скачано.'),
+            main_text=ru('<b>✅ Обновление скачано.</b>'),
             main_keyboard=kb,
         )
