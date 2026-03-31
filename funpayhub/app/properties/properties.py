c36d30adcf5d75211307198aa2b660c8308e66fd
qvvonk <qvvonk@gmail.com>
2026-03-31 11:24:57 +0300

    version bump to 0.5.12

--- funpayhub/app/properties/properties.py
+++ funpayhub/app/properties/properties.py
@@ -35,7 +35,7 @@ class FunPayHubProperties(Properties):
                 id='version',
                 name='version',
                 description='version',
-                value='0.5.11',
+                value='0.5.12',
             ),
         )
         self.toggles = self.attach_node(TogglesProperties())
